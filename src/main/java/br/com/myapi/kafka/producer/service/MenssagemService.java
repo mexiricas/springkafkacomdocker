822fe30297faa52d08e4d8037fb083ba2bec7829
mexiricas <mexiricas@hotmail.com>
2022-06-22 20:30:37 -0300

    Producer inserindo arquivo com alguns comandos kafka e class Service

--- src/main/java/br/com/myapi/kafka/producer/service/MenssagemService.java
+++ src/main/java/br/com/myapi/kafka/producer/service/MenssagemService.java
@@ -5,7 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class MenssagemService {
 	private static final Logger logger	= LoggerFactory.getLogger(MenssagemService.class);
 	
